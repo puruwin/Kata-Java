d4e6e5094c1c0ce2c029a6b257b8f7798093e081
David Pérez Martínez <38837156+puruwin@users.noreply.github.com>
2018-10-02 10:10:51 +0200

    Add files via upload

---
+++ PixelArtAlt.java
@@ -0,0 +1,11 @@
+public class Kata {
+
+  public static boolean isDivisible(int wallLength, int pixelSize) {
+  if(wallLength % pixelSize == 0)
+    return true;
+  else 
+    return false;
+    
+  }
+
+}
